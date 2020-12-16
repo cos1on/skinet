58c3d06059c516cd56f398b3ed2239f630549757
Umut Olcay <uolcay_1907@outlook.com>
2020-12-16 17:49:10 +0300

    Initial Commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+
+        // creating data base 
+        public DbSet<Product> Products { get; set; }
+    }
+}
