99718caca1b38c3f058a8ff5f8f6312e3a7671cd
Umut Olcay <uolcay_1907@outlook.com>
2020-12-18 21:50:06 +0300

    End of section 3

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    // databasedeki tablelardaki degerleri ayarliyoruz burda
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        void IEntityTypeConfiguration<Product>.Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.PictureUrl).IsRequired();
+            builder.HasOne(b => b.ProductBrand).WithMany()
+                           .HasForeignKey(p => p.ProductBrandId);
+            builder.HasOne(b =>b.ProductType).WithMany()
+                           .HasForeignKey(p => p.ProductTypeId);
+        }
+    }
+}
